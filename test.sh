9b9d09ef3214610349986f1e063fe2b1765b9022
Daniel Shuy <17351764+daniel-shuy@users.noreply.github.com>
2025-08-21 17:12:57 +0800

    Publish as danielshuy/config-merge (#2)
    Publish to Docker Hub and GHCR

--- test.sh
+++ test.sh
@@ -5,47 +5,47 @@ set -e
 echo "JSON:"
 docker run --rm \
     -v "$(pwd)/test/mix/:/home/node/" \
-    boxboat/config-merge \
+    danielshuy/config-merge \
     -f json "*"
 echo ""
 
 echo "TOML:"
 docker run --rm \
     -v "$(pwd)/test/mix/:/home/node/" \
-    boxboat/config-merge \
+    danielshuy/config-merge \
     -f toml "*"
 echo ""
 
 echo "YAML array merge:"
 docker run --rm \
     -v "$(pwd)/test/mix/:/home/node/" \
-    boxboat/config-merge \
+    danielshuy/config-merge \
     "*"
 echo ""
 
 echo "YAML array overwrite:"
 docker run --rm \
     -v "$(pwd)/test/mix/:/home/node/" \
-    boxboat/config-merge \
+    danielshuy/config-merge \
     -a overwrite "*"
 echo ""
 
 echo "YAML array concat:"
 docker run --rm \
     -v "$(pwd)/test/mix/:/home/node/" \
-    boxboat/config-merge \
+    danielshuy/config-merge \
     -a concat "*"
 echo ""
 
 echo "Docker Compose"
 docker run --rm \
     -v "$(pwd)/test/docker-compose/:/home/node/" \
-    boxboat/config-merge \
+    danielshuy/config-merge \
     local.env docker-compose.yml docker-compose-local.patch.yml docker-compose-local.yml
 echo ""
 
 echo "Docker Compose no envsubst"
 docker run --rm \
     -v "$(pwd)/test/docker-compose/:/home/node/" \
-    boxboat/config-merge \
+    danielshuy/config-merge \
     --no-envsubst local.env docker-compose.yml docker-compose-local.patch.yml docker-compose-local.yml
